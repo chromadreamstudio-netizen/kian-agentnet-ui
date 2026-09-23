ed9683226d5846824bb2d249cf2a01e05002081a
Walid <chromadreamstudio@gmail.com>
2026-09-23 06:31:51 +0300

    fix: escape apostrophe entity in login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -88,7 +88,7 @@ export default function LoginPage() {
         </form>
 
         <div className="mt-6 text-center text-sm text-gray-400">
-          Don't have an account? <Link href="/signup" className="text-blue-400 hover:text-blue-300 transition">Sign up</Link>
+          Don&apos;t have an account? <Link href="/signup" className="text-blue-400 hover:text-blue-300 transition">Sign up</Link>
         </div>
       </div>
     </div>
